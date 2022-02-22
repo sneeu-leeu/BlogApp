c2013a383168a72ed3fdd35e30f3d0e1f935d9cc
sneeu-leeu <sa070197@gmail.com>
2022-02-22 10:37:27 +0200

    create post counter updater for User

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author ,class_name: "User"
   has_many :comments
   has_many :likes
+
+  def update_post_counter(arg)
+    User.update('post_counter', arg)
+  end
+
 end
