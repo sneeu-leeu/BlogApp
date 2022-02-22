8eaa7ac318ca325be2b8ae23e8a993523b99f699
sneeu-leeu <sa070197@gmail.com>
2022-02-22 10:26:25 +0200

    update database and moddels

--- db/migrate/20220217100338_create_posts.rb
+++ db/migrate/20220217100338_create_posts.rb
@@ -1,11 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: 'users' }
       t.string :title
       t.text :text
 
       t.timestamps
+      t.integer :comments_counter, :default => 0
+      t.integer :posts_counter, :default => 0
     end
   end
 end
--- db/migrate/20220217100752_add_posts_counter_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddPostsCounterToPosts < ActiveRecord::Migration[6.1]
-  def change
-    add_column :posts, :posts_counter, :integer, :default => 0
-  end
-end
