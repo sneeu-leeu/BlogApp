9f6ec74db824bafa7096f428c4078e0d262b2340
sneeu-leeu <sa070197@gmail.com>
2022-02-24 11:14:25 +0200

    create forms for post, likes , comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,23 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @author = @post.author
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    new_post = current_user.posts.new(params.require(:post).permit(:title, :text))
+    respond_to do |format|
+      format.html do
+        if new_post.save
+          redirect_to "/users/#{new_post.author_id}/posts/"
+        else
+          render :new
+        end
+      end
+    end
+  end
 end
+
+
