77eb3ffc99254802ad070f8acea33efecb6c1964
sneeu-leeu <sa070197@gmail.com>
2022-02-24 11:41:34 +0200

    style app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html do
         if new_post.save
-          redirect_to "/users/#{new_post.author_id}/posts/"
+          redirect_to "/users/#{new_post.author.id}/posts/"
         else
           render :new
         end
