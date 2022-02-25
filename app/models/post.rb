e88cc27d9893394469dcee1241f2ebd4dae93449
sneeu-leeu <sa070197@gmail.com>
2022-02-25 15:39:08 +0200

    Create specs for model tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,14 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 } 
+
   def load_recent_five_comments
     comments.order('created_at Desc').limit(5)
   end
 
   after_save :update_posts_counter
-
-  private
 
   def update_posts_counter
     author.increment!(:posts_counter)
