319b1c7934a8469648bc1afa7a9a0d0aa0c562cc
sneeu-leeu <sa070197@gmail.com>
2022-02-28 14:26:52 +0200

    create models for quiries

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :comments
   has_many :likes
+
+
+  after_save :update_post_counter
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def load_recent_five_comments
+    comment.order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'user_id'
   has_many :comments, foreign_key: 'user_id'
   has_many :likes, foreign_key: 'user_id'
+
+  def load_recent_three_posts
+    post.includes(:commenst).order(created_at: :desc).limit(3)
+  end
 end
