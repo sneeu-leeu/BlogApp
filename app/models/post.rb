4009b542255162f5a6189545516e7e0ee6baaa67
sneeu-leeu <sa070197@gmail.com>
2022-03-09 23:03:24 +0200

    Add delete button to post views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-  has_many :comments
-  has_many :likes
+  has_many :comments, depndant: :destroy
+  has_many :likes, depndant: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
